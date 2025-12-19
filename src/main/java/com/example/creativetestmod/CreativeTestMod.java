a2e1ba8fdf43160bf925d0b2a08108ddfae7a6bc
TheRealBasic <59488774+TheRealBasic@users.noreply.github.com>
2025-12-19 23:58:44 +0000

    Remove wrapper jar from repository

--- src/main/java/com/example/creativetestmod/CreativeTestMod.java
+++ src/main/java/com/example/creativetestmod/CreativeTestMod.java
@@ -1,58 +1,28 @@
 package com.example.creativetestmod;
 
-import com.example.creativetestmod.proxy.CommonProxy;
-import com.example.creativetestmod.tweaks.SkyLightHandler;
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.Mod.Instance;
-import cpw.mods.fml.common.SidedProxy;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.registry.GameRegistry;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Items;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemFood;
-import net.minecraft.item.ItemStack;
-import net.minecraft.potion.Potion;
-import net.minecraftforge.common.MinecraftForge;
+import com.mojang.logging.LogUtils;
+import net.minecraft.SharedConstants;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLLoader;
+import org.slf4j.Logger;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
-public class CreativeTestMod {
+/**
+ * Minimal entrypoint for the Creative Test Mod targeting Minecraft 1.21.11.
+ */
+@Mod(CreativeTestMod.MOD_ID)
+public final class CreativeTestMod {
+    public static final String MOD_ID = "creative_test_mod";
+    private static final Logger LOGGER = LogUtils.getLogger();
 
-    @Instance(Reference.MOD_ID)
-    public static CreativeTestMod instance;
-
-    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
-    public static CommonProxy proxy;
-
-    public static Item glowingApple;
-
-    public static final CreativeTabs creativeTab = new CreativeTabs(Reference.MOD_ID) {
-        @Override
-        public Item getTabIconItem() {
-            return Items.glowstone_dust;
-        }
-    };
-
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
-        glowingApple = new ItemFood(6, 0.8F, false)
-                .setPotionEffect(Potion.nightVision.id, 30, 0, 1.0F)
-                .setAlwaysEdible()
-                .setUnlocalizedName("glowingApple")
-                .setTextureName(Reference.MOD_ID + ":glowing_apple")
-                .setCreativeTab(creativeTab);
-
-        GameRegistry.registerItem(glowingApple, "glowingApple");
-        LogHelper.info("Glowing apple registered.");
+    public CreativeTestMod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
     }
 
-    @EventHandler
-    public void init(FMLInitializationEvent event) {
-        proxy.registerRenderers();
-
-        GameRegistry.addShapelessRecipe(new ItemStack(glowingApple), new ItemStack(Items.apple), new ItemStack(Items.glowstone_dust));
-        MinecraftForge.EVENT_BUS.register(new SkyLightHandler());
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        LOGGER.info("Creative Test Mod initialized for Minecraft {} on Forge {}", SharedConstants.getCurrentVersion().getName(), FMLLoader.versionInfo().forgeVersion());
     }
 }
